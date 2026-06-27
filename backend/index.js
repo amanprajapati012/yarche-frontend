d5732eef660194226801e53f7d4dc572b1e98f05
Aman <amanprajapati265482@gmail.com>
2026-06-27 16:50:50 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -9,36 +9,41 @@ const { initSocket } = require("./socket/socket");
 
 const app = express();
 
-// 🔥 1. CORS FIRST (IMPORTANT)
+// Static folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// DB connect
+db();
+
+// Middleware
+app.use(express.json());
+
+// ✅ ONLY YOUR FRONTEND IP
 app.use(
   cors({
     origin: [
-      "https://yarche-frontend.vercel.app",
+       "https://yarche-frontend.vercel.app",
       "http://localhost:3000",
       "http://192.168.29.170:3000",
       "http://192.168.1.39:8081",
     ],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    methods: [
+      "GET",
+      "POST",
+      "PUT",
+      "PATCH",
+      "DELETE",
+      "OPTIONS",
+    ],
     allowedHeaders: [
       "Content-Type",
       "Authorization",
-      "adminauthorization",
+       "adminauthorization",
     ],
     credentials: true,
   })
 );
 
-// 🔥 2. Handle preflight requests (VERY IMPORTANT)
-app.options("*", cors());
-
-// Static folder
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-// Body parser
-app.use(express.json());
-
-// DB connect
-db();
 
 // Routes
 app.use(router);
@@ -46,6 +51,9 @@ app.use("/admin", adminRouter);
 
 // Server start
 const PORT = process.env.PORT || 5000;
+
+// 🔥 IMPORTANT
+
 
 const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
