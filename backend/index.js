203cd23a78abde0fd88ba0b8fd28f5f225d1ccd7
Aman Prajapati <amanprajapati265482@gmail.com>
2026-08-09 16:55:17 +0530

    all Pages Completed

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(
     origin: [
       "https://yarche-frontend.vercel.app",
       "http://localhost:3000",
-      "http://192.168.29.172:3000",
+      "http://10.21.213.215",
       "http://192.168.1.39:8081",
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
